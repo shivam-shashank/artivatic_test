31ae2a18281976c49b813497e6e7bd3bbdd817d6
Shivam Kumar <shashank.1@iitj.ac.in>
2022-04-26 18:11:02 +0530

    Api call Done.

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,10 +1,53 @@
+import 'package:artivatic_test/features/home/bloc/home_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    context.read<HomeBloc>().add(LoadHomeDataEvent());
+
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        if (state is HomeInitialState) {
+          return const Scaffold(
+            body: Center(
+              child: Text("Home Initial State."),
+            ),
+          );
+        }
+        if (state is HomeLoadingState) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        if (state is HomeLoadedState) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(state.dataModelResult.title),
+              centerTitle: true,
+            ),
+            body: ListView.builder(
+              itemCount: state.dataModelResult.rows.length,
+              itemBuilder: (context, index) {
+                return const Card();
+              },
+            ),
+          );
+        }
+        if (state is HomeErrorState) {
+          return Scaffold(
+            body: Center(
+              child: Text(state.error.toString()),
+            ),
+          );
+        }
+        return Container();
+      },
+    );
   }
 }
